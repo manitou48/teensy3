b429c919b7fbb0dc2db2c5db94ab7b3e99b290fc
manitou48 <manitou@home>
2015-04-25 20:30:45 -0400

    DAC DMA FTM1 on LC working

--- dacDMA.ino
+++ dacDMA.ino
@@ -1,11 +1,9 @@
-// dacDMA v2  test DMA out with DAC and timer and DMAChannel
+// dacDMA v3  test DMA out with DAC and timer and DMAChannel on LC
 //  DAC in DMA mode, DMAMAX source is FTM1, FTM1 at frequency
 // regular DMA buffer or try circular buffer (align)
 //  could do triangle, sinewave,  or faux PWM  hi low
 //  test jumper a12 to a0
 //  could do ascii plot (see pyboard dacadc.py)
-//   enable interrupts?  clear state?  need DMA_DCR_CS?
-//  see analog.c
 
 #include <DMAChannel.h>
 
@@ -18,8 +16,12 @@ DMAChannel dma(false);
 uint16_t data[N];
 
 void prregs() {
-	PRREG(DMA_DSR_BCR0);
+    PRREG(DMA_DSR_BCR0);
     PRREG(DMA_DCR0);
+    PRREG(FTM1_SC);
+    PRREG(DAC0_C0);
+    PRREG(DAC0_DAT0L);
+    PRREG(DAC0_DAT0H);
 }
 
 volatile int DMAdone;
@@ -29,40 +31,31 @@ void dma_isr() {
 	dma.clearInterrupt();
 }
 
-volatile unsigned long ticks;
-
-void ftm1_isr(void) {
-	FTM1_SC |= FTM_SC_TOF;  // reset interrupt
-	ticks++;
-}
 
 void timer_init() {
 	// ftm1 channel 0
-	NVIC_ENABLE_IRQ(IRQ_FTM1); 
 	SIM_SCGC6 |= SIM_SCGC6_TPM1;
-	FTM1_SC = 0;
+	FTM1_SC = 0; delay(1);
 	FTM1_CNT = 0;
 	FTM1_MOD = (F_PLL/2)/FREQ - 1;
 	FTM1_C0V = 0;
-	FTM1_C0SC = FTM_CSC_DMA;    // DMA enable channel 0
-	FTM1_SC = FTM_SC_CLKS(1) | FTM_SC_PS(0)  | FTM_SC_TOF | FTM_SC_TOIE ;
+	FTM1_SC =  0x100 | FTM_SC_CLKS(1) | FTM_SC_PS(0)  | FTM_SC_TOF;
 }
 
 void dac_init() {
 	dma.begin(true);
 
-    SIM_SCGC6 |= SIM_SCGC6_DAC0;
-	DAC0_C0 = DAC_C0_DACEN;
-	timer_init();
+	SIM_SCGC6 |= SIM_SCGC6_DAC0;
+	DAC0_C0 = DAC_C0_DACEN | DAC_C0_DACRFS;	
 
 	// DMA init
-	dma.sourceBuffer(data,N);
-	dma.destination((volatile uint8_t&)DAC0_DAT0L);
+	dma.sourceBuffer(data,sizeof(data));
+	dma.destination((volatile uint16_t&)DAC0_DAT0L);
 	dma.disableOnCompletion();
-	dma.triggerAtHardwareEvent(DMAMUX_SOURCE_FTM1_CH0);
-	prregs();
+	dma.triggerAtHardwareEvent(DMAMUX_SOURCE_FTM1_OV);
+	//prregs();
 	dma.enable();
-	dma.attachInterrupt(dma_isr);
+	timer_init();
 }
 
 
@@ -71,9 +64,9 @@ void setup() {
 	int i;
 
 	analogWriteResolution(12);
+	analogReadAveraging(8);
 	Serial.begin(9600);
 	while(!Serial);
-	delay(4000);
 	Serial.println("jumper A0 to A12");
 	for (i=0;i<N;i++) data[i]=4*i;
 	t1 = micros();
@@ -88,13 +81,16 @@ void setup() {
 void loop() {
 	unsigned long t1;
 
-	prregs();
+//	prregs();
 	t1=micros();
-	while(!DMAdone);
+		delay(500); // bout halfway
+		Serial.println(analogRead(A0));
+		delay(1);
+		Serial.println(analogRead(A0));
+	while(!dma.complete());
 	t1 = micros() - t1;
-	DMAdone=0;
-	Serial.print(t1); Serial.print("us ");
+	Serial.print(t1); Serial.print(" us  A0= ");
 	Serial.println(analogRead(A0));
-	Serial.println(ticks);
-	delay(3000);
+	//prregs();
+	while(1);  // stop
 }
