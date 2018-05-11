f1fb66a725ddebf110c62de31a89a3f577c9d682
manitou48 <manitou@home>
2018-05-11 06:23:02 -0400

    update

--- dacadc.ino
+++ dacadc.ino
@@ -1,4 +1,5 @@
-// DAC out (A14  A12 LC) to ADC A0 in   pin 12 for hi/lo
+// DAC out T3.2 A14  (A12 LC) to ADC A0 in   pin 12 for hi/lo  K66 A21 or A22
+#define DACPIN A22
 #define VREF 3.3
 
 void setup() {
@@ -6,34 +7,34 @@ void setup() {
   analogWriteResolution(12);
   analogReadResolution(16);
   analogReadAveraging(32);
-  pinMode(12,INPUT);  // voltage threshold
+  pinMode(12, INPUT); // voltage threshold
 }
 void loop() {
-  int i, sensorValue,k;
+  int i, sensorValue, k;
   float v;
   char str[80];
 
-  for (i=0; i< 4096; i+=40) {
-	analogWrite(A14,i);
-	//  ? should we wait for DAC to settle 2-15us, datasheet
-	delayMicroseconds(10);
-	sensorValue = analogRead(A0);
-    v= VREF * sensorValue/65536.;
-	k = sensorValue>>4;
-    sprintf(str,"%d  %d %d   %d ",i,k,sensorValue,digitalRead(12));
-	Serial.print(str);
-	Serial.println(v,3);
+  for (i = 0; i < 4096; i += 40) {
+    analogWrite(DACPIN, i);
+    //  ? should we wait for DAC to settle 2-15us, datasheet
+    delayMicroseconds(10);
+    sensorValue = analogRead(A0);
+    v = VREF * sensorValue / 65536.;
+    k = sensorValue >> 4;
+    sprintf(str, "%d  %d %d   %d ", i, k, sensorValue, digitalRead(12));
+    Serial.print(str);
+    Serial.println(v, 3);
     delay(1000);
   }
-  for (i=4096-40; i >= 0; i-=40) {
-	analogWrite(A14,i);
-	delayMicroseconds(10);
-	sensorValue = analogRead(A0);
-    v= VREF * sensorValue/65536.;
-	k = sensorValue>>4;
-    sprintf(str,"%d  %d %d   %d ",i,k,sensorValue,digitalRead(12));
-	Serial.print(str);
-	Serial.println(v,3);
+  for (i = 4096 - 40; i >= 0; i -= 40) {
+    analogWrite(DACPIN, i);
+    delayMicroseconds(10);
+    sensorValue = analogRead(A0);
+    v = VREF * sensorValue / 65536.;
+    k = sensorValue >> 4;
+    sprintf(str, "%d  %d %d   %d ", i, k, sensorValue, digitalRead(12));
+    Serial.print(str);
+    Serial.println(v, 3);
     delay(1000);
   }
 }
